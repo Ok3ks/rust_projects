4fcf9bf71ea566767cdd84641d6051ac3c85321e
Emmanuel Okedele <emmanuel@coefficient.ai>
2025-01-18 12:19:11 +0000

    Additional methods to asynchronously download parquet files from cdns.

--- create_api/src/backend/data.rs
+++ create_api/src/backend/data.rs
@@ -35,3 +35,51 @@ pub async fn get_fake_trips(_from_ms: i64, n_results: i64) -> Result<Vec<Trip>> 
     
     Ok(trips)
 }
+
+pub async fn get_trips(_from_ms: i64, n_results: i64) -> Result<Vec<Trip>> {
+    let (year, month) = get_year_and_month(from_ms);
+    info!("Extracted year: {}, month: {}", year, month);
+
+    info!("Downloading parquet file for year: {}, month: {}");
+    let file_path = download_parquet_file(year, month).await?;
+
+    //Get the trips from the file
+    let trips = get_trips_from_file(&file_path, _from_ms, n_results)?;
+
+    info!("Returning {} trips", trips.len());
+    Ok(trips)
+}
+
+pub fn get_year_and_month(_from_ms:i64) -> (i32, i32) {
+    let datetime = DateTime::<Utc>::from_timestamp(_from_ms/ 1000, 0).unwrap();
+    (datetime.year(), datetime.month() as i32)
+}
+
+pub async fn download_parquet_file(year: i32, month:i32) -> Result<String> {
+    
+    let url = format!(
+        "https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_{}-{:02}.parquet",
+        year,
+        month
+    );
+
+    let file_path = format!("yellow_tripdata_{}-{:02}.parquet", year, month);
+    
+    //Check if the file exists before trying to download
+    if tokio::fs::try_exists(&file_path).await? {
+        info!("File {} already exists", &file_path);
+        return Ok(file_path);
+    }
+
+    info!("Downloading file from {}", &url);
+    let response = reqwest::get(&url).await?;
+    if response.status().is_success() {
+        let byte = response.bytes().await?;
+
+        tokio::fs::write(&file_path, bytes).await?
+        info!("File {} downloaded successfully", &file_path);
+    } else {
+        error!("Failed to download file");
+    }
+    Ok(file_path)
+}
