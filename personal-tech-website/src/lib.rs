2516caeed757de41b5829513a69b704ae9310d02
Emmanuel Okedele <emmanuel@coefficient.ai>
2025-08-01 15:07:46 +0100

    full migration to 0.8.6

--- personal-tech-website/src/lib.rs
+++ personal-tech-website/src/lib.rs
@@ -3,8 +3,8 @@ pub mod app;
 #[cfg(feature = "hydrate")]
 #[wasm_bindgen::prelude::wasm_bindgen]
 pub fn hydrate() {
-    use app::*;
-    use leptos::*;
+    use app::App;
+    use leptos::mount::mount_to_body;
 
     console_error_panic_hook::set_once();
 
